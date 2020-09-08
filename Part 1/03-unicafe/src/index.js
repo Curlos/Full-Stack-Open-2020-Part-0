072b68ec5d5aec88cdf97a884be13dafffd67953
curlos <curlosmart@gmail.com>
2020-09-08 14:38:08 -0400

    1.10: unicafe step5 (complete)

--- Part 1/03-unicafe/src/index.js
+++ Part 1/03-unicafe/src/index.js
@@ -1,40 +1,13 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-// Gather statistics
-const Statistics = (props) => {
-  const good = props.values[0]
-  const neutral = props.values[1]
-  const bad = props.values[2]
-  const total = good + neutral + bad;
-  const average = (good + neutral + bad) / 3;
-  let positive = !isNaN(good / total) ? good / total * 100 : 0;
-
-  if (total === 0) {
-    return (
-      <div>
-        No feedback given
-      </div>
-    )
-  }
-
-  return (
-    <div>
-      <div>{props.text[0]} {good}</div>
-      <div>{props.text[1]} {neutral}</div>
-      <div>{props.text[2]} {bad}</div>
-      <div>all {total}</div>
-      <div>average {average}</div>
-      <div>positive {positive}%</div>
-    </div>
-  )
-}
 
 const App = () => {
   // save clicks of each button to own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const total = (good + neutral + bad) / 3
 
   const handleGoodClick = () => {
     setGood(good + 1)
@@ -66,6 +39,51 @@ const Button = ({ handleClick, text }) => {
     <button onClick={handleClick}>{text}</button>
   )
 }
+
+const Statistic = ({ text, value }) => {
+  if (text === 'positive') {
+    return (
+      <div>
+        {text} {value}%
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      {text} {value}
+    </div>
+  )
+}
+
+const Statistics = (props) => {
+  const good = props.values[0]
+  const neutral = props.values[1]
+  const bad = props.values[2]
+  const total = good + neutral + bad;
+  const average = (good + neutral + bad) / 3;
+  let positive = !isNaN(good / total) ? good / total * 100 : 0;
+
+  if (total === 0) {
+    return (
+      <div>
+        No feedback given
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      <Statistic text="good" value={good} />
+      <Statistic text="neutral" value={neutral} />
+      <Statistic text="bad" value={bad} />
+      <Statistic text="total" value={good + neutral + bad} />
+      <Statistic text="average" value={total / 3} />
+      <Statistic text="positive" value={!isNaN(good / total) ? good / total * 100 : 0} />
+    </div>
+  )
+}
+
 
 ReactDOM.render(<App />,
   document.getElementById('root')
