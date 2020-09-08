2ad28073a6f9130b4a4936342b73551b1b59367b
curlos <curlosmart@gmail.com>
2020-09-08 11:56:09 -0400

    Part 1 Component state, event handlers complete

--- Part 1/01-part1/src/index.js
+++ Part 1/01-part1/src/index.js
@@ -1,24 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-const Hello = (props) => {
-  return (
-    <div>
-      <p>Hello {props.name}, you are {props.age}</p>
-    </div>
-  )
-}
-
 const App = () => {
-  const name = 'Peter';
-  const age = 10;
+  const [counter, setCounter] = useState(0);
+
+  const increaseByOne = () => setCounter(counter + 1);
+
+  const setToZero = () => setCounter(0);
+
+  const decreaseByOne = () => setCounter(counter - 1);
+
   return (
     <div>
-      <h1>Greetings</h1>
-      <Hello name="Maya" age={26 + 10} />
-      <Hello name={name} age={age} />
+      <Display counter={counter} />
+      <Button handleClick={increaseByOne} text="plus" />
+      <Button handleClick={setToZero} text="zero" />
+      <Button handleClick={decreaseByOne} text="minus" />
     </div>
   )
 }
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const Display = ({ counter }) => <div>{counter}</div>
+
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
+
+ReactDOM.render(<App />, document.getElementById('root'));
