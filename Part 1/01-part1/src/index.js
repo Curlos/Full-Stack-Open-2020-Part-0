c527301146ba031d37eefbfc7d43c491b6868334
curlos <curlosmart@gmail.com>
2020-09-08 13:12:41 -0400

    Part1 A more complex state, debugging React apps

--- Part 1/01-part1/src/index.js
+++ Part 1/01-part1/src/index.js
@@ -1,31 +1,29 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-const App = () => {
-  const [counter, setCounter] = useState(0);
+const Display = (props) => <div>{props.value}</div>
 
-  const increaseByOne = () => setCounter(counter + 1);
+const Button = (props) => (
+  <button onClick={props.handleClick}>
+    {props.text}
+  </button>
+)
 
-  const setToZero = () => setCounter(0);
+const App = props => {
+  const [value, setValue] = useState(10)
 
-  const decreaseByOne = () => setCounter(counter - 1);
+  const setToValue = newValue => {
+    setValue(newValue)
+  }
 
   return (
     <div>
-      <Display counter={counter} />
-      <Button handleClick={increaseByOne} text="plus" />
-      <Button handleClick={setToZero} text="zero" />
-      <Button handleClick={decreaseByOne} text="minus" />
+      <Display value={value} />
+      <Button handleClick={() => setToValue(1000)} text="thousand" />
+      <Button handleClick={() => setToValue(0)} text="reset" />
+      <Button handleClick={() => setToValue(value + 1)} text="increment" />
     </div>
   )
 }
-
-const Display = ({ counter }) => <div>{counter}</div>
-
-const Button = ({ handleClick, text }) => (
-  <button onClick={handleClick}>
-    {text}
-  </button>
-)
 
 ReactDOM.render(<App />, document.getElementById('root'));
