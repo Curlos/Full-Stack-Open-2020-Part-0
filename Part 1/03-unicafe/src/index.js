4ed3bf6ce3016835129235cd25d1eaed660b3a36
curlos <curlosmart@gmail.com>
2020-09-08 14:00:43 -0400

    1.6: unicafe step1 (complete)

--- Part 1/03-unicafe/src/index.js
+++ Part 1/03-unicafe/src/index.js
@@ -7,9 +7,59 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const handleGoodClick = () => {
+    setGood(good + 1)
+  }
+
+  const handleNeutralClick = () => {
+    setNeutral(neutral + 1)
+  }
+
+  const handleBadClick = () => {
+    setBad(bad + 1)
+  }
+
   return (
     <div>
-      code here
+      <ButtonHeader />
+      <Button handleClick={handleGoodClick} text="good" />
+      <Button handleClick={handleNeutralClick} text="neutral" />
+      <Button handleClick={handleBadClick} text="bad" />
+
+      <StatisticsHeader />
+      <Statistics value={good} text="good" />
+      <Statistics value={neutral} text="neutral" />
+      <Statistics value={bad} text="bad" />
+    </div>
+  )
+}
+
+const ButtonHeader = (props) => {
+  return (
+    <div>
+      <h1>give feedback</h1>
+    </div>
+  )
+}
+
+const Button = ({ handleClick, text }) => {
+  return (
+    <button onClick={handleClick}>{text}</button>
+  )
+}
+
+const StatisticsHeader = (props) => {
+  return (
+    <div>
+      <h1>statistics</h1>
+    </div>
+  )
+}
+
+const Statistics = (props) => {
+  return (
+    <div>
+      {props.text} {props.value}
     </div>
   )
 }
