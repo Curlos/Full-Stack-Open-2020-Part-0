c45cabeef6af041d25c151d049a6db67e3c0607b
curlos <curlosmart@gmail.com>
2020-09-08 22:59:17 -0400

    2.3*: Course information step8 (complete)

--- Part 2/02-courseinfo/src/index.js
+++ Part 2/02-courseinfo/src/index.js
@@ -74,7 +74,7 @@ const Course = ({ course }) => {
         <div>
             <Header course={course} />
             <Content course={course} />
-
+            <Total course={course} />
         </div>
     )
 }
