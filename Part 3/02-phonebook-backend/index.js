fe33bc1127bd84a86f3b5471ba9764b486edbe91
Carlos Martinez <curlosmart@gmail.com>
2020-09-27 16:46:19 -0400

    3.15: Phonebook database, step3 (complete)

--- Part 3/02-phonebook-backend/index.js
+++ Part 3/02-phonebook-backend/index.js
@@ -64,16 +64,12 @@ app.get("/api/persons/:id", (request, response) => {
 });
 
 app.delete("/api/persons/:id", (request, response) => {
-	const id = Number(request.params.id);
-	persons = persons.filter((person) => person.id !== id);
-
-	response.status(204).end();
+	Person.findByIdAndRemove(request.params.id)
+		.then((result) => {
+			response.status(204).end();
+		})
+		.catch((error) => next(error));
 });
-
-const generateId = () => {
-	const randomID = Math.floor(Math.random() * 100000) + 1;
-	return randomID;
-};
 
 app.post("/api/persons", (request, response) => {
 	const body = request.body;
