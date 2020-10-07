9e210050eff1cbf2d5eec2e28f0c74b310f2f392
Carlos Martinez <curlosmart@gmail.com>
2020-10-06 22:28:27 -0400

    5.7* Blog list frontend, step7 (complete)

--- Part 5/bloglist-frontend-real/src/components/Blog.js
+++ Part 5/bloglist-frontend-real/src/components/Blog.js
@@ -1,8 +1,43 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import React, { useState } from "react";
 
-export default Blog
+const Blog = ({ blog }) => {
+	const [visible, setVisible] = useState(false);
+
+	const hideWhenVisible = { display: visible ? "none" : "" };
+	const showWhenVisible = { display: visible ? "" : "none" };
+
+	const toggleVisibility = () => {
+		setVisible(!visible);
+	};
+
+	const blogStyle = {
+		paddingTop: 10,
+		paddingLeft: 2,
+		border: "solid",
+		borderWidth: 1,
+		marginBottom: 5,
+	};
+
+	const buttonStyle = {
+		margin: "6px",
+	};
+
+	return (
+		<div style={blogStyle}>
+			{blog.title} {blog.author}
+			<button onClick={toggleVisibility} style={buttonStyle}>
+				{visible === false ? "view" : "hide"}
+			</button>
+			<div style={showWhenVisible}>
+				<p>{blog.url}</p>
+				<p>
+					likes {blog.likes}
+					<button style={buttonStyle}>like</button>
+				</p>
+				<p>{blog.author}</p>
+			</div>
+		</div>
+	);
+};
+
+export default Blog;
