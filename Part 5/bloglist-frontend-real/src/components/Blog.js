5273634e0bc16e63fbcbda06d5d51902b8413888
Carlos Martinez <curlosmart@gmail.com>
2020-10-08 16:38:52 -0400

    5.13: Blog list tests, step1 (complete)

--- Part 5/bloglist-frontend-real/src/components/Blog.js
+++ Part 5/bloglist-frontend-real/src/components/Blog.js
@@ -1,79 +1,85 @@
-import React, { useState } from 'react'
-import blogService from '../services/blogs'
-import PropTypes from 'prop-types'
+import React, { useState } from "react";
+import blogService from "../services/blogs";
+import PropTypes from "prop-types";
 
 const Blog = ({ blog, user }) => {
-    const [visible, setVisible] = useState(false)
-    const [likes, setLikes] = useState(blog.likes)
+	const [visible, setVisible] = useState(false);
+	const [likes, setLikes] = useState(blog.likes);
 
-    const showWhenVisible = { display: visible ? '' : 'none' }
+	const showWhenVisible = { display: visible ? "" : "none" };
 
-    const toggleVisibility = () => {
-        setVisible(!visible)
-    }
+	const toggleVisibility = () => {
+		setVisible(!visible);
+	};
 
-    const blogStyle = {
-        paddingTop: 10,
-        paddingLeft: 2,
-        border: 'solid',
-        borderWidth: 1,
-        marginBottom: 5,
-    }
+	const blogStyle = {
+		paddingTop: 10,
+		paddingLeft: 2,
+		border: "solid",
+		borderWidth: 1,
+		marginBottom: 5,
+	};
 
-    const buttonStyle = {
-        margin: '6px',
-    }
+	const buttonStyle = {
+		margin: "6px",
+	};
 
-    const incrementLikes = async () => {
-        const changedBlog = { ...blog, likes: likes + 1 }
+	const incrementLikes = async () => {
+		const changedBlog = { ...blog, likes: likes + 1 };
 
-        try {
-            await blogService.incrementLikes(blog.id, changedBlog)
-            setLikes(likes + 1)
-        } catch (exception) {
-            console.log(exception)
-        }
-    }
+		try {
+			await blogService.incrementLikes(blog.id, changedBlog);
+			setLikes(likes + 1);
+		} catch (exception) {
+			console.log(exception);
+		}
+	};
 
-    const removeBlog = async () => {
-        if (window.confirm(`Remove blog ${blog.title} by ${blog.author}`)) {
-            try {
-                await blogService.deleteBlog(blog.id)
-            } catch (exception) {
-                console.log(exception)
-            }
-        }
-    }
+	const removeBlog = async () => {
+		if (window.confirm(`Remove blog ${blog.title} by ${blog.author}`)) {
+			try {
+				await blogService.deleteBlog(blog.id);
+			} catch (exception) {
+				console.log(exception);
+			}
+		}
+	};
 
-    return (
-        <div style={blogStyle}>
-            {blog.title} {blog.author}
-            <button onClick={toggleVisibility} style={buttonStyle}>
-                {visible === false ? 'view' : 'hide'}
-            </button>
-            <div style={showWhenVisible}>
-                <p>{blog.url}</p>
-                <p>
+	console.log(blog);
+
+	return (
+		<div style={blogStyle}>
+			<div className="authorAndTitle">
+				<p>
+					{blog.title} {blog.author}
+				</p>
+				<button onClick={toggleVisibility} style={buttonStyle}>
+					{visible === false ? "view" : "hide"}
+				</button>
+			</div>
+			<div style={showWhenVisible} className="likesAndUrl">
+				<p>{blog.url}</p>
+				<p>
 					likes {likes}
-                    <button style={buttonStyle} onClick={incrementLikes}>
+					<button style={buttonStyle} onClick={incrementLikes}>
 						like
-                    </button>
-                </p>
-                <p>{blog.author}</p>
-                {user.username === blog.user.username &&
+					</button>
+				</p>
+				<p>{blog.author}</p>
+				{user.username === blog.user.username &&
 				user.name === blog.user.name ? (
-                        <button onClick={removeBlog}>remove</button>
-                    ) : (
-                        ''
-                    )}
-            </div>
-        </div>
-    )
-}
+					<button onClick={removeBlog}>remove</button>
+				) : (
+					""
+				)}
+			</div>
+		</div>
+	);
+};
 
 Blog.propTypes = {
-    blog: PropTypes.object.isRequired,
-    user: PropTypes.object.isRequired,
-}
+	blog: PropTypes.object.isRequired,
+	user: PropTypes.object.isRequired,
+};
 
-export default Blog
+export default Blog;
---
+++ Part 5/bloglist-frontend-real/src/components/Blog.test.js
@@ -0,0 +1,73 @@
+import React from "react";
+import "@testing-library/jest-dom/extend-expect";
+import { render } from "@testing-library/react";
+import Blog from "./Blog";
+
+test("renders content", () => {
+	const blog = {
+		title: "4 factors that could help Heat upset Lakers75",
+		author: "Sekou Smith",
+		url:
+			"https://www.nba.com/article/2020/09/29/4-factors-how-heat-can-upset-lakers",
+		user: {
+			id: "5f7b6cbde09c2b458380405",
+			name: "Carlos Martinez",
+			username: "curlos",
+		},
+	};
+
+	const user = {
+		username: "curlos",
+		name: "Carlos Martinez",
+	};
+
+	const component = render(<Blog blog={blog} user={user} />);
+});
+
+test("renders the blog's title and author", () => {
+	const blog = {
+		title: "4 factors that could help Heat upset Lakers75",
+		author: "Sekou Smith",
+		url:
+			"https://www.nba.com/article/2020/09/29/4-factors-how-heat-can-upset-lakers",
+		user: {
+			id: "5f7b6cbde09c2b458380405",
+			name: "Carlos Martinez",
+			username: "curlos",
+		},
+	};
+
+	const user = {
+		username: "curlos",
+		name: "Carlos Martinez",
+	};
+
+	const component = render(<Blog blog={blog} user={user} />);
+
+	expect(component.container.querySelector(".authorAndTitle")).toBeDefined();
+});
+
+test("by default likes and url are not rendered", () => {
+	const blog = {
+		title: "4 factors that could help Heat upset Lakers75",
+		author: "Sekou Smith",
+		url:
+			"https://www.nba.com/article/2020/09/29/4-factors-how-heat-can-upset-lakers",
+		user: {
+			id: "5f7b6cbde09c2b458380405",
+			name: "Carlos Martinez",
+			username: "curlos",
+		},
+	};
+
+	const user = {
+		username: "curlos",
+		name: "Carlos Martinez",
+	};
+
+	const component = render(<Blog blog={blog} user={user} />);
+
+	const div = component.container.querySelector(".likesAndUrl");
+
+	expect(div).toHaveStyle("display: none");
+});
