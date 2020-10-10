8984ec191e260eb2a6eb6bb564dcf6cf12aa5920
Carlos Martinez <curlosmart@gmail.com>
2020-10-10 07:22:11 -0400

    5.18: bloglist end to end testing, step2 (✓)

--- Part 5/bloglist-frontend-real/cypress/integration/blog_app.spec.js
+++ Part 5/bloglist-frontend-real/cypress/integration/blog_app.spec.js
@@ -13,10 +13,32 @@ describe("Blog app", function () {
 	it("Login form is shown", function () {
 		// ...
 		cy.contains("login").click();
-		cy.get("#username").type("mluukkai");
-		cy.get("#password").type("salainen");
-		cy.get("#login-button").click();
+	});
 
-		cy.contains("Matti Luukkainen logged-in");
+	describe("Login", function () {
+		it("succeeds with correct credentials", function () {
+			// ...
+			cy.contains("login").click();
+			cy.get("#username").type("mluukkai");
+			cy.get("#password").type("salainen");
+			cy.get("#login-button").click();
+
+			cy.contains("Matti Luukkainen logged-in");
+		});
+
+		it("fails with wrong credentials", function () {
+			// ...
+			cy.contains("login").click();
+			cy.get("#username").type("mluukkai");
+			cy.get("#password").type("wrong");
+			cy.get("#login-button").click();
+
+			cy.get(".error")
+				.should("contain", "Wrong credentials")
+				.and("have.css", "color", "rgb(255, 0, 0)")
+				.and("have.css", "border-style", "solid");
+
+			cy.get("html").should("not.contain", "Matti Luukkainen logged in");
+		});
 	});
 });
