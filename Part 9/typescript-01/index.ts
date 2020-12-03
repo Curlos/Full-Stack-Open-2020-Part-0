9ea0bff45394265aff9d84cb1aa7565783c11e00
Carlos Martinez <curlosmart@gmail.com>
2020-12-03 17:43:24 -0500

    typescript-01 (notes for 9.6 and 9.7)

--- Part 9/typescript-01/index.ts
+++ Part 9/typescript-01/index.ts
@@ -1,8 +1,18 @@
 import express from 'express';
+import {calculator} from './calculator';
+import {Operation} from './calculator'
+
 const app = express();
 
 app.get('/ping', (_req, res) => {
   res.send('pong');
+});
+
+app.get('/calculate', (req, res) => {
+  const { value1, value2, op } = req.query
+
+  const result = calculator(Number(value1), Number(value2), op);
+  res.send(result);
 });
 
 const PORT = 3003;
