8bcba0fb381f86f36b4ec8276e5e99d9b8de5e9f
Carlos Martinez <curlosmart@gmail.com>
2020-12-18 20:53:35 -0500

    9.20: patientor, step5 (complete)

--- Part 9/patientor/src/components/PatientInfo.tsx
+++ Part 9/patientor/src/components/PatientInfo.tsx
@@ -4,31 +4,29 @@ import { Patient } from "../types";
 const PatientInfo: React.FC<{patient: Patient | null | undefined}> = ({patient}) => {
   if(patient) {
     console.log(patient)
-    if(patient.gender === "male") {
-      return (
-        <div>
-          <h1>{patient.name} <i className="male icon"></i></h1>
-          <p>ssn: {patient.ssn}</p>
-          <p>occupation: {patient.occupation}</p>
-        </div>
-      )
-    } else if (patient.gender === "female") {
-      return (
-        <div>
-          <h1>{patient.name} <i className="female icon"></i></h1>
-          <p>ssn: {patient.ssn}</p>
-          <p>occupation: {patient.occupation}</p>
-        </div>
-      )
-    } else {
-      return (
-        <div>
-          <h1>{patient.name}</h1>
-          <p>ssn: {patient.ssn}</p>
-          <p>occupation: {patient.occupation}</p>
-        </div>
-      )
-    }
+    return (
+      <div>
+        <h1>
+          {patient.name} {patient.gender === "male" ? <i className="male icon" /> : <i className="female icon"/>}
+        </h1>
+        <p>ssn: {patient.ssn}</p>
+        <p>occupation: {patient.occupation}</p>
+
+        <h2>entries</h2>
+        {patient.entries.map((entry, i) => {
+          console.log(entry)
+          return (
+            <div key={i}>
+              <p key={i}>{entry.date} <i>{entry.description}</i></p>
+              <ul>
+                {entry.diagnosisCodes?.map((diagnosisCode, i) => <li key={i}>{diagnosisCode}</li>)}
+              </ul>
+            </div>
+            
+          )
+        })}
+      </div>
+    )
   } else {
     return null;
   }
