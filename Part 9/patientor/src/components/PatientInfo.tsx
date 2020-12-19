43304a82a33b1cecac685a9da8c752564a8aa8bb
Carlos Martinez <curlosmart@gmail.com>
2020-12-18 22:15:23 -0500

    9.22: patientor, step7 (complete)

--- Part 9/patientor/src/components/PatientInfo.tsx
+++ Part 9/patientor/src/components/PatientInfo.tsx
@@ -1,5 +1,77 @@
 import React from "react";
-import { Patient, Diagnosis } from "../types";
+import { Patient, Diagnosis, Entry } from "../types";
+import { Card, Icon, Image, Container } from 'semantic-ui-react'
+
+const assertNever = (value: never): never => {
+  throw new Error(
+    `Unhandled discriminated union member: ${JSON.stringify(value)}`
+  );
+};
+
+const EntryDetails: React.FC<{ entry: Entry }> = ({ entry }) => {
+  switch(entry.type) {
+    case "Hospital":
+      return (
+        
+        <Card>
+          <Card.Content>
+            <Card.Header>{entry.date} <Icon name='hospital' size='big'/></Card.Header>
+            <Card.Meta>
+              <span className='date'>Joined in 2015</span>
+            </Card.Meta>
+            <Card.Description>
+              {entry.description}
+            </Card.Description>
+          </Card.Content>
+          <Card.Content extra>
+            <a>
+              <Icon name='heart' />
+            </a>
+          </Card.Content>
+        </Card>
+      )
+    case "OccupationalHealthcare":
+      return (
+        <Card>
+          <Card.Content>
+            <Card.Header>{entry.date} <Icon name='user md' size='big'/></Card.Header>
+            <Card.Meta>
+              <span className='date'>Joined in 2015</span>
+            </Card.Meta>
+            <Card.Description>
+              {entry.description}
+            </Card.Description>
+          </Card.Content>
+          <Card.Content extra>
+            <a>
+              <Icon name='heart' />
+            </a>
+          </Card.Content>
+        </Card>
+      )
+    case "HealthCheck":
+      return (
+        <Card>
+          <Card.Content>
+            <Card.Header>{entry.date} <Icon name='stethoscope' size='big'/></Card.Header>
+            <Card.Meta>
+              <span className='date'>Joined in 2015</span>
+            </Card.Meta>
+            <Card.Description>
+              {entry.description}
+            </Card.Description>
+          </Card.Content>
+          <Card.Content extra>
+            <a>
+              <Icon name='heart' />
+            </a>
+          </Card.Content>
+        </Card>
+      )
+    default:
+      return assertNever(entry);
+  }
+}
 
 const PatientInfo: React.FC<{patient: Patient | null | undefined, diagnoses: { [code: string]: Diagnosis }}> = ({patient, diagnoses}) => {
   
@@ -17,21 +89,7 @@ const PatientInfo: React.FC<{patient: Patient | null | undefined, diagnoses: { [
         {patient.entries.map((entry, i) => {
           return (
             <div key={i}>
-              <p key={i}>{entry.date} <i>{entry.description}</i></p>
-              <ul>
-                {entry.diagnosisCodes?.map((diagnosisCode, i) => {
-                  const diagnosis = Object.values(diagnoses).find(diagnosis => diagnosis.code === diagnosisCode);
-                  let diagnosisName = '';
-
-                  if(diagnosis) {
-                    diagnosisName = diagnosis.name;
-                  }
-                  
-                  return (
-                    <li key={i}>{diagnosisCode} {diagnosisName}</li>
-                  )
-                })}
-              </ul>
+              <EntryDetails entry={entry} />
             </div>
             
           )
