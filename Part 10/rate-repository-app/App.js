84a8a26930cc04c87d4f208a0d97df5c28327694
Carlos Martinez <curlosmart@gmail.com>
2021-07-16 17:15:54 -0400

    10.1 (Complete)

--- Part 10/rate-repository-app/App.js
+++ Part 10/rate-repository-app/App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Space Jam 2 out in theaters on July 16, 2021.</Text>
       <StatusBar style="auto" />
     </View>
   );
