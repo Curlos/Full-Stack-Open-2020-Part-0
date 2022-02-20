f5c4b54c4b9c8124ce83333e405562305664ac2f
Carlos Martinez <curlosmart@gmail.com>
2022-02-20 15:22:26 -0500

    changes

--- Part 10/rate-repository-app/src/components/RepositoryItem.jsx
+++ Part 10/rate-repository-app/src/components/RepositoryItem.jsx
@@ -1,19 +1,72 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Image, StyleSheet, View } from 'react-native';
+import Text from './Text';
 
 const RepositoryItem = ({ repository }) => {
 
+    const stars = repository.item.stargazersCount > 1000 ? (Math.round((repository.item.stargazersCount / 1000) * 10) / 10).toFixed(1)+ 'k' : repository.item.stargazersCount;
+
+    const forks = repository.item.forksCount > 1000 ? (Math.round((repository.item.forksCount / 1000) * 10) / 10).toFixed(1) + 'k' : repository.item.forksCount;
+
+    const viewStyles = StyleSheet.create({
+        container: {
+            backgroundColor: "#ffffff",
+        }
+    });
+
+    const avatarStyles = StyleSheet.create({
+        container: {
+            width: 50,
+            height: 50,
+            borderRadius: 10,
+        }
+    });
+
+    const styles = StyleSheet.create({
+        container: {
+            display: "flex",
+            flexDirection: "row"
+        },
+        fullName: {
+            fontWeight: "bold",
+        },
+        description: {
+            color: "#282829"
+        },
+        language: {
+            backgroundColor: "#0366d6",
+            padding: 20,
+            color: "#ffffff",
+            borderRadius: 10,
+        }
+    });
+
     return (
-        <Text>
-            {"\n"}
-            Full name: {repository.item.fullName}{"\n"}
-            Description: {repository.item.description}{"\n"}
-            Language: {repository.item.language}{"\n"}
-            Stars: {repository.item.forksCount}{"\n"}
-            Forks: {repository.item.stargazersCount}{"\n"}
-            Reviews: {repository.item.ratingAverage}{"\n"}
-            Rating: {repository.item.reviewCount}{"\n"}
-        </Text>
+        <View style={viewStyles.container}>
+            <Text>
+                {"\n"}
+                <Image 
+                    source={{ uri: repository.item.ownerAvatarUrl }}
+                    style={avatarStyles.container}
+                />
+                <Text style={styles.container}>
+                    <Text style={styles.fullName}>
+                        {repository.item.fullName}
+                    </Text>
+                    <Text style={styles.description}>
+                        {repository.item.description}
+                    </Text>
+                </Text>{"\n"}
+                <Text style={styles.language}>
+                    {repository.item.language}
+                </Text>
+                {"\n"}
+                Stars: {stars}{"\n"}
+                Forks: {forks}{"\n"}
+                Reviews: {repository.item.ratingAverage}{"\n"}
+                Rating: {repository.item.reviewCount}{"\n"}
+            </Text>
+        </View>
     );
 };
 
