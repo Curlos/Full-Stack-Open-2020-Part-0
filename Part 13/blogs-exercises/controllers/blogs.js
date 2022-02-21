022f16a88dbb3beb194b662e87c53979de358f2c
Carlos Martinez <curlosmart@gmail.com>
2022-02-21 15:08:12 -0500

    13.14 (Complete)

--- Part 13/blogs-exercises/controllers/blogs.js
+++ Part 13/blogs-exercises/controllers/blogs.js
@@ -35,9 +35,10 @@ router.get('/', async (req, res) => {
     const where = {}
 
     if (req.query.search) {
-      where.title = {
-        [Op.substring]: req.query.search
-      }
+      where[Op.or] = [
+        { title: { [Op.substring]: req.query.search } },
+        { author: { [Op.substring]: req.query.search } }
+      ]
     }
 
     console.log(where)
